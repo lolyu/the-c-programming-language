21bb2efb24ae81cb3a04e5356e4d05afa913ae82
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2019-05-10 13:08:06 +0800

    Create 1-10.c

---
+++ chapter1/1-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int c;
+    for (; (c = getchar()) != EOF;) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else
+            putchar(c);
+    }
+}
