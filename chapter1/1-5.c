6a2922e21b8fe10f107634fbbb3b7c14e04bf88b
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2019-05-10 12:47:30 +0800

    Update 1-5.c

--- chapter1/1-5.c
+++ chapter1/1-5.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    int b;
-    char c;
-    printf("EOF: %d\n", EOF);
-    while (1) {
-        b = ((c = getchar()) != EOF);
-        printf("c = %c, (getchar() != EOF) = %d\n", c, b);
-        if (!b)
-            break;
-    }
+    int fahr;
+    for (fahr = 300; fahr >=0; fahr -= 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
