798baf522bf39d65f08ecc0c5ab341abe82a5c62
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2019-05-12 21:37:53 +0800

    Update 1-20.c

--- chapter1/1-20.c
+++ chapter1/1-20.c
@@ -9,14 +9,25 @@
 int main(int argc, char* argv[]) {
 	int i;
 	int c;
-	int retval;
-	
-	while ((c = getchar()) != EOF)
-		if (c == TAB)
-			for (i = 0; i < TABSIZE; ++i)
+	int count;
+
+	printf("-------------------------------------\n");
+	count = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == TAB) {
+			for (i = 0; i < TABSIZE - count; ++i)
 				putchar(SPACE);
-		else
+			count = 0;
+		}
+		else if (c == '\n') {
 			putchar(c);
+			count = 0;
+		}
+		else {
+			putchar(c);
+			count = (count + 1) % TABSIZE;
+		}
+	}
 
 	return 0;
 }
