83d43519e22c181845de2d3c2c2990d418f14c83
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2019-05-28 14:56:42 +0800

    Update and rename 5-1.c to 5-01.c

---
+++ chapter5/5-01.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void);
+void ungetch(int);
+int getint(int *pn);
+
+int main() {
+    int n;
+    while (getint(&n) > 0)
+        printf("GET: %d\n", n);
+    return 0;
+}
+
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        fprintf(stderr, "ungetch: too many characters.\n");
+    else
+        buf[bufp++] = c;
+}
+
+int getint(int *pn) {
+    int c, sign;
+    while (isspace(c = getch()));
+    if (!isdigit(c) && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        c = getch();
+        if (!isdigit(c)) {
+            ungetch(sign == 1 ? '+' : '-');
+            return 0;
+        }
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
