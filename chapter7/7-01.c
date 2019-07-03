f3e4883ab28b5240155b6de57c80cb0bc97621d1
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2019-07-03 17:04:41 +0800

    Update 7-01.c

--- chapter7/7-01.c
+++ chapter7/7-01.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+int main(int argc, char *argv[]) {
+    int (*fp)(int) = NULL;
+    int c, i;
+    for (i = 0; i < strlen(argv[0]); tolower(argv[0][i++]));
+    if (strcmp(argv[0], "./tolower") == 0)
+        fp = &tolower;
+    else if (strcmp(argv[0], "./toupper") == 0)
+        fp = &toupper;
+    else {
+        fprintf(stderr, "unsupported op");
+        exit(EXIT_FAILURE);
+    }
+    while ((c = getchar()) != EOF)
+        putchar((*fp)(c));
+    exit(EXIT_SUCCESS);
+}
